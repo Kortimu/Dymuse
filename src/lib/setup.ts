ed948888df0fff56023a9a6fb5ed596651924b72
Kortimu <mareks.greizans05@gmail.com>
2023-05-19 20:37:02 +0300

    fix: prevent duplicate application commands from being registered

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -10,6 +10,7 @@ import { config } from 'dotenv-cra';
 import { join } from 'path';
 import { inspect } from 'util';
 import { srcDir } from './constants';
+import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
 
 // Read env var
 config({ path: join(srcDir, '.env') });
@@ -19,3 +20,6 @@ inspect.defaultOptions.depth = 1;
 
 // Enable colorette
 colorette.createColors({ useColor: true });
+
+// Make Discord not store useless duplicates of the same command
+ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite)
