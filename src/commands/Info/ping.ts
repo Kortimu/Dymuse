730022faadf28d192af9c0875573b6d6daff4e2b
Kortimu <mareks.greizans05@gmail.com>
2023-07-21 00:03:43 +0300

    feat: improve ping command

--- src/commands/Info/ping.ts
+++ src/commands/Info/ping.ts
@@ -19,9 +19,8 @@ export class UserCommand extends BotCommand {
   }
 
   public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-    await sendLoadingInteraction(interaction);
-    const message = interaction.options.data;
-    console.log(message);
+    const loadInteraction = await sendLoadingInteraction(interaction);
+    const message = await interaction.fetchReply();
 
     const response = await interaction.editReply({
       embeds: [
@@ -32,34 +31,31 @@ export class UserCommand extends BotCommand {
               name: 'Bot latency (very cringe):',
               value: `${Math.round(this.container.client.ws.ping)} ms`,
             },
-            // {
-            //   name: 'Framework latency (equally boring):',
-            //   value: `${
-            //     (loadInteraction.createdTimestamp) -
-            //     (loadInteraction.createdTimestamp)
-            //   } ms`,
-            // },
+            {
+              name: 'Framework latency (equally boring):',
+              value: `${message.createdTimestamp - loadInteraction.createdTimestamp} ms`,
+            },
           )
           .setColor('#00FF00'),
       ],
     });
     try {
-      await response
-        .awaitReactions({
-          filter: (collected) => collected.emoji.name === '🏓',
-        })
-        .then((collected) => {
-          console.log(collected);
-          return interaction.editReply({
-            embeds: [
-              baseEmbedFormat()
-                .setTitle('WOAH! A RESPONSE... FROM YOU!!!')
-                .setDescription(
-                  'Look at you, actually caring about me. Not many speak to me, so for this wonderful occasion, [here is a cool video that will lighten your mood.](https://www.youtube.com/watch?v=dQw4w9WgXcQ)',
-                ),
-            ],
-          });
+      const collector = response.createReactionCollector({
+        filter: (collected) => collected.emoji.name === '🏓',
+        time: 30 * 1000,
+        max: 1,
+      });
+      collector.on('collect', () => {
+        return interaction.editReply({
+          embeds: [
+            baseEmbedFormat()
+              .setTitle('WOAH! A RESPONSE... FROM YOU!!!')
+              .setDescription(
+                'Look at you, actually caring about me. Not many speak to me, so for this wonderful occasion, [here is a cool video that will lighten your mood.](https://www.youtube.com/watch?v=dQw4w9WgXcQ)',
+              ),
+          ],
         });
+      });
     } catch (e) {
       console.log(e);
       return;
